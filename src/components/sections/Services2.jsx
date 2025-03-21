788a97809afbd540a3329279a9939aadc2e364c2
ilutechnologies <admin@ilutechnologies.com>
2025-03-21 17:09:38 +0530

    4th update

--- src/components/sections/Services2.jsx
+++ src/components/sections/Services2.jsx
@@ -7,29 +7,6 @@ const Services2Section = ( { introHidden, noPaddingBottom } ) => {
         {/* Onovo Services */}
         <section className={noPaddingBottom ? "onovo-section gap-top-140" : "onovo-section gap-top-140 gap-bottom-140"}>
             <div className="container">
-                {introHidden == undefined &&
-                    <>
-                        {/* Heading */}
-                        <div className="onovo-heading gap-bottom-40 align-center">
-                            <div className="onovo-subtitle-1">
-                                <span dangerouslySetInnerHTML={{__html: Data.subtitle}} />
-                            </div>
-                            <h2 className="onovo-title-2">
-                                <span dangerouslySetInnerHTML={{__html: Data.title}} />
-                            </h2>
-                        </div>
-
-                        {/* Description */}
-                        <div className="row gap-bottom-60">
-                            <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6 offset-lg-3 align-center">
-                                <div className="onovo-text">
-                                    <p dangerouslySetInnerHTML={{__html: Data.text}} />
-                                    <p><img src={Data.signature.image} alt={Data.signature.alt} /></p>
-                                </div>
-                            </div>
-                        </div>
-                    </>
-                }
 
                 {/* Services items */}
                 <div className="row gap-row">
